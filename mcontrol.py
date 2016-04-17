a60fc5c7ca78f481a03a2ee7b43eedea6e8e79b1
Matt Arriola <matt.arriola87@gmail.com>
2016-04-17 19:03:57 -0400

    Removing debug print statements

--- mcontrol.py
+++ mcontrol.py
@@ -15,21 +15,18 @@ class MediaControl(Gtk.Window):
         self.labels += map(lambda x: self.glade.get_object("label" + x), list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 
         css = Gtk.CssProvider()
-        print("css")
         display = Gdk.Display.get_default()
-        print("display")
         screen = Gdk.Display.get_default_screen(display)
-        print("screen")
 
         Gtk.StyleContext.add_provider_for_screen(screen, css, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-        print("add provider")
         css.load_from_path("mcontrol.css")
-        print("load")
 
         self.window.show_all()
 
-print("*" * 80)
+    def winMControl_hide(self, sender):
+        Gtk.main_quit()
         
+
 if __name__ == "__main__":
     win = MediaControl()
     Gtk.main()
